b696cc6164f4396d065c1a8c60e9e6f5f93cfb5b
Victor <62649152+vicw0ng-hk@users.noreply.github.com>
2021-04-20 01:17:49 +0800

    Built basic app

--- app/app.py
+++ app/app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_wtf.csrf import CSRFProtect
+import os
 
+SECRET_KEY = os.urandom(32)
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
+csrf = CSRFProtect(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/result/', methods=['POST'])
+def result():
+    url = request.form['url']
+    title = request.form['title']
+    authors = request.form['authors']
+    content = request.form['content']
+    context = {'url': url, 'title': title, 'authors': authors, 'content': content}
+    return render_template('result.html', context=context)
 
 if __name__ == "__main__":
     app.run(debug=True)
