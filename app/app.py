d559a7ca78e4dd54c4bcccde217fdcb9002d58e7
vicw0ng-hk <62649152+vicw0ng-hk@users.noreply.github.com>
2021-04-21 21:34:15 +0800

    Continued to build app

--- app/app.py
+++ app/app.py
@@ -8,8 +8,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 csrf = CSRFProtect(app)
 
-# learn = load_learner('app/model/model.pkl')
-print('************************')
+learn = load_learner(app.root_path + '/model/model.pkl')
+vocab = ['bias', 'clickbait', 'conspiracy', 'fake', 'hate', 
+         'junksci', 'political', 'reliable', 'satire', 'unreliable']
 
 @app.route('/')
 def index():
@@ -22,8 +23,16 @@ def result():
     authors = request.form['authors']
     content = request.form['content']
     text = ' | '.join([url, title, authors, content])
-    context = {'url': url, 'title': title, 'authors': authors, 'content': content}
+    res = learn.predict(text)[2].tolist()
+    prob = {vocab[i]: res[i] for i in range(10)}
+    ranking = sorted(prob.items(), key=lambda x: x[1], reverse=True)
+    context = {'url': url, 'title': title, 'authors': authors, 'text': text, 'pred': ranking}
     return render_template('result.html', context=context)
+
+@app.route('/hidden/', methods=['POST'])
+def hidden():
+    print(request.form)
+    return 'Feedback recorded!'
 
 if __name__ == "__main__":
     app.run(debug=True)
